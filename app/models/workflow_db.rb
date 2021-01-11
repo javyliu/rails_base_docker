7946fc8d491d8ca515ce7aa22c94141a22305cd6
qmliu <qmliu@pearlinpalm.com>
2021-01-11 09:26:46 +0800

    add drails command for 'docker-compose exec web xxxx'

--- app/models/workflow_db.rb
+++ app/models/workflow_db.rb
@@ -1,5 +1,5 @@
 class WorkflowDb < ActiveRecord::Base
   self.abstract_class = true
-  #connects_to database: {}
-  establish_connection :workflow
+  connects_to database: {writing: :workflow }
+  #establish_connection :workflow
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_103016) do
+ActiveRecord::Schema.define(version: 2021_01_09_024715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2021_01_07_103016) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "novels", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "book_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
