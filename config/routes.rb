599d6089bccfd68d69622051cf131449aac715e9
qmliu <qmliu@pearlinpalm.com>
2021-01-11 16:32:32 +0800

    add kaminari

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :novels
-  resources :articles
+
+  concern :paginatable do
+    get '(page/:page)', action: :index, on: :collection, as: ''
+  end
+  resources :articles,concerns: :paginatable
+
   root "articles#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
