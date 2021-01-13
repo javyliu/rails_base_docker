c7531b27700f03339143184929f5bce12fb10e9e
qmliu <qmliu@pearlinpalm.com>
2021-01-13 13:17:53 +0800

    update files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
   resources :novels
 
@@ -7,5 +8,9 @@ Rails.application.routes.draw do
   resources :articles,concerns: :paginatable
 
   root "articles#index"
+
+  mount Sidekiq::Web => '/sikekiq'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
